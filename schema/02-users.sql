a02b7030b2bb4bc461f5d97499e9fea5b8b3539d
Salvador Guzman <guzmansalv@gmail.com>
2021-03-20 09:24:53 -0700

    wrong type

--- schema/02-users.sql
+++ schema/02-users.sql
@@ -2,7 +2,7 @@
 
 create table duolingo.data.users (
     insert_id BIGSERIAL NOT NULL PRIMARY KEY,
-    insert_time TIMESTAMPZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    insert_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     id BIGINT NOT NULL UNIQUE,
     username text NOT NULL
 );
